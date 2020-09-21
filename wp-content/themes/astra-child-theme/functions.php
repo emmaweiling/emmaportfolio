cf1df77ad1ef5de298242ea4012820860cb07729
emmaweiling <56149715+emmaweiling@users.noreply.github.com>
2020-09-22 01:42:43 +0200

    upload_2
    have glemt at ændre 'astra-style' for astra theme

--- wp-content/themes/astra-child-theme/functions.php
+++ wp-content/themes/astra-child-theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parenthandle = 'parent-style'; // This is 'astra-style' for the astra theme.
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
         array(),  // if the parent theme code has a dependency, copy it to here
